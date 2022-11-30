86ad9c6c1c7137e0b771c5e26f36310c4239c857
Denis <de9404@gmail.com>
2022-11-30 21:04:27 +0300

    Отмена идеи использовать два массива. Пересобираем один и делаем Resize

--- Program.cs
+++ Program.cs
@@ -21,28 +21,23 @@ void FillArray(string[] array) // заполняем массив
     }
 }
 
-
-void FilterArray(string[] array, string[] array2) //фильтруем массив по условию x<=3 символа и собираем новый
+void FilterArray(string[] array) //фильтруем массив по условию x<=3 символа и собираем новый
 {
     int count = 0;
     for(int i = 0; i<array.Length;i++)
     {
         if(array[i].Length<=3)
-            count++;
-    }
-    if(count>0)
-        Array.Resize(ref array2, count);
-    else Console.WriteLine("Во введёном массиве нет элементов, подходящих условиям.");
-    count = 0;
-    for(int i = 0; i<array.Length;i++)
-    {
-        if(array[i].Length<=3)
         {
-            array[i] = array2[count];
-            Console.Write(array2[count]);
+            array[count] = array[i];
             count++;
         }
     }
+    if(count>0)
+    {
+        Array.Resize(ref array, count);
+        Console.WriteLine(array.Length);
+    }
+    else Console.WriteLine("Во введёном массиве нет элементов, подходящих условиям.");
 }
 
 
@@ -53,4 +48,4 @@ int size = Convert.ToInt32(Console.ReadLine());
 string[] MyArray = new string[size];
 string[] FiltredArray = new string[size];
 FillArray(MyArray);
-FilterArray(MyArray,FiltredArray);
+FilterArray(MyArray);
